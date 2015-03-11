b88b42ed7c663dd864bcbe016f06ab04f85ba1dc
Wiktor Sztajerowski <w2iktor@gmail.com>
2015-03-11 18:22:11 +0100

    #15 update create_user script

--- backend/create_user.js
+++ backend/create_user.js
@@ -5,21 +5,19 @@ var config 			= require('./libs/config');
 
 var args = process.argv.slice(2);
 
-var login = args[0];
+var email = args[0];
 var password = args[1];
 var role = args[2] || 'ADMIN';
-var name = args[3] || login;
-var email = args[4] || "test@gmail.com";
+var name = args[3] || 'Test user'
 
 if (args.length < 2) {
     console.log("usage: node %s %s %s", path.basename(process.argv[1]), "user", "password");
     process.exit();
 }
 
-console.log("Username: %s", login);
+console.log("E-mail: %s", email);
 console.log("Password: %s", password);
 console.log("Name: %s", name);
-console.log("E-mail: %s", email);
 
 console.log("Creating a new user in Mongo");
 
@@ -34,8 +32,6 @@ mongoose.connection.once('open', function callback() {
     console.log("Mongoose connected to the database");
 
     var user = new User();
-
-    user.login = login;
     user.password = password;
     user.name = name;
     user.email = email;
