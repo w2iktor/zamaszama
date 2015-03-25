ff453fb382b563953d72c51b79c9f9b1a8b4bd09
tomaszszlek <tomasz.szlek@gmail.com>
2015-03-25 07:51:03 +0100

    #16  Current users orders - get more information about saved meal from server

--- frontend/app/user/currentOrder/currentOrder.js
+++ frontend/app/user/currentOrder/currentOrder.js
@@ -9,15 +9,18 @@ angular.module('zamaszamaApp.userCurrentOrder', ['ngRoute', 'ngResource'])
       });
     }])
 
-.controller('UserCurrentOrderController', function ($scope, UserCurrentOrderFactory, MealsFactory, $location) {
+.controller('UserCurrentOrderController', function ($scope, UserCurrentOrderFactory, MealsFactory, $filter, $location) {
       $scope.sendOrder = function () {
           var choosenMeals = {meals:[]};
               angular.forEach($scope.currentOrder.meals, function(meal, key) {
                   if (meal.amount > 0) {
                       var newMeal = {}
                       newMeal.amount = meal.amount;
-                      newMeal.meal = meal.name;
-                      newMeal._id = meal._id
+                      newMeal.meal = meal._id;
+                      //newMeal.price = meal.price;
+                      //newMeal.type = meal.type;
+                      //newMeal.name = meal.name;
+                      //newMeal.company = meal.company;
                       choosenMeals.meals.push(newMeal);
                   }
               });
@@ -41,23 +44,35 @@ angular.module('zamaszamaApp.userCurrentOrder', ['ngRoute', 'ngResource'])
         $scope.deleteItem = function (selectedMenuItem, list) {
             list.splice( selectedMenuItem, 1 );
         };
-        var savedCurrentOrder = UserCurrentOrderFactory.show(function(response){
-            if($scope.currentOrder.message == "No order"){
-                $scope.currentOrder = {meals:[]}
-            }
-            else {
-                //TODO : Verify if code below works when current order crud will be fixed
-                $scope.currentOrder = {meals:[]}
-                angular.forEach(savedCurrentOrder.meals, function(meal, key) {
-                    if (meal.amount > 0) {
-                        var newMeal = {}
-                        newMeal.amount = meal.amount;
-                        newMeal.meal = meal.name;
-                        newMeal._id = meal._id
-                        $scope.currentOrder.meals.push(newMeal);
-                    }
-                });
-            }
+
+        $scope.menu = MealsFactory.query(function(){
+            var savedCurrentOrder = UserCurrentOrderFactory.show(function(response){
+                if(savedCurrentOrder.message == "No order"){
+                    $scope.currentOrder = {meals:[]}
+                }
+                else {
+                    //TODO : Verify if code below works when current order crud will be fixed
+                    $scope.currentOrder = {meals:[]}
+                    angular.forEach(savedCurrentOrder.meals, function(meal, key) {
+                        if (meal.amount > 0) {
+                            var newMeal = {}
+
+
+                            var fullMealInfo = $filter('filter')($scope.menu, {_id: meal._id})[0];
+
+
+                            newMeal.amount = meal.amount;
+                            newMeal._id = meal.meal;
+
+                            newMeal.price = fullMealInfo.price;
+                            newMeal.type = fullMealInfo.type;
+                            newMeal.name = fullMealInfo.name;
+                            newMeal.company = fullMealInfo.company;
+                            $scope.currentOrder.meals.push(newMeal);
+                        }
+                    });
+                }
+            });
         });
-        $scope.menu = MealsFactory.query();
+
     });
